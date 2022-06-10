741595e0827bb0384723d35078810e46a4550904
Agam Kohli <agamkohli9@gmail.com>
2022-06-10 10:41:05 -0400

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-and not set -q TMUX
-	exec tmux
 end
--- fish/functions/patch.fish
+++ fish/functions/patch.fish
@@ -1,7 +1,7 @@
 # How to contribute to Linux Kernel:
 # 1) run "$patch" to find TODOS in drivers/staging
 # 2) go on agam-patch branch
-# 3) check errors on <file> with "$~/github/staging/scripts/checkpatch.pl -f --terse <file>"
+# 3) check errors on <file> with "$~/github/linux/scripts/checkpatch.pl -f --terse <file>"
 # 4) commit changes with "$git commit -a"
 # 	Staging: <driver>: <file_without_extension>: <summary>
 # 	
@@ -9,11 +9,11 @@
 #
 # 	Signed-off-by: Agam Kohli <agamkohli9@gmail.com>
 # 5) run "$git format-patch master..agam-patch"
-# 6) mutt -H <patch_file>
-# 	send to: gregkh@linuxfoundation.org, cc: devel@driverdev.osuosl.org, linux-kernel@vger.kernel.org
+# 6) git send-email 
+#	send to: gregkh@linuxfoundation.org, cc: $(~/github/linux/scripts/get_maintainer.pl *.patch)
 
 function patch
-cd /home/agam/github/staging/drivers/staging/
+cd /home/agam/github/linux/
 clear
-find . -name "TODO" -print0 | xargs -0 grep "checkpatch.pl"
+find drivers/staging -name "TODO" -print0 | xargs -0 grep "checkpatch.pl"
 end
